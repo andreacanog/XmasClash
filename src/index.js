4ff4d4e305e64d4944175a81ff3bd3de9685355d
Andrea Cano <andreacanogisbert@gmail.com>
2022-12-07 09:06:23 -0800

    added css to my game

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import Game from './game.js'
 window.onload = function () {
     let startGameModal = document.getElementById("startGameModal");
 
-    // Get the <span> element that closes the modal
+    
     let playButton = document.getElementsByClassName("gamePlay")[0];
 
-    // When the user clicks on the button, open the modal
-    // btn.onclick = function() {
-    //     startGameModal.style.display = "block";
-    // }
 
-    // When the user clicks on <span> (x), close the modal
-    playButton.onclick = function() {
-        startGameModal.style.display = "none";
-    }
+    // playButton.onclick = function() {
+    //     startGameModal.style.display = "none";
+        
+    //     const board = new Board()
+    //     board.startGame();
+    //     window.setInterval( function() {
+    //         board.clash();
+    //         board.slideDownBall();
+    //         board.populateBall();
+    //     }, 30)
+    // }
 
     // When the user clicks anywhere outside of the modal, close it
     // window.onclick = function(event) {
@@ -26,11 +29,20 @@ window.onload = function () {
     // }
     // let canvas = document.getElementById("Xmas-Clash")
     // const board = new Board(canvas)
-    const board = new Board()
-    board.startGame();
-    window.setInterval( function() {
-        board.clash();
-        board.slideDownBall();
-        board.populateBall();
-    }, 30)
+
+
+    document.addEventListener("click", (event) => {
+        if (event.target.classList.contains("gamePlay")){
+            startGameModal.style.display = "none";
+
+            const board = new Board()
+            board.startGame();
+            window.setInterval( function() {
+                board.clash();
+                board.slideDownBall();
+                board.populateBall();
+            }, 30)
+        }
+    });
+   
 }
